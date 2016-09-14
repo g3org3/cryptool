967d0d24cb391050e54f0f163114e41d8125a48d
g3org3 <7jagjag@gmail.com>
2016-09-14 00:12:02 -0600

    update cmd with help

--- cryptool.sh
+++ cryptool.sh
@@ -1,32 +1,49 @@
 #!/bin/bash
 
 usage() {
-  echo -e "\nUSAGE:";
-  echo "./cryptool.sh "
-  echo -e "\n Options:"
+  echo -e "\n USAGE:";
   echo -e "  ./cryptool.sh --encrypt <filename> <public_key>"
   echo -e "  ./cryptool.sh --decrypt <filename> <private_key> <tar.gz>"
+  echo -e "\n More info"
+  echo -e "  public_key\t PKCS8 public key"
+  echo -e "  filename\t Path of the file to encrypt"
+  echo -e "  tar.gz\t Input for decrypting"
+  echo -e "\n FAQ"
+  echo -e "  common error: unable to load Public Key"
+  echo -e "  fix -> ssh-keygen -e -f ~/.ssh/id_rsa.pub -m PKCS8 > ~/.ssh/id_rsa.pub.pkcs8"
+  echo -e "  or you can run ./cryptool.sh --fix <public_key>"
   echo -e ""
 }
 
-if [[ "$1" = "--encrypt" ]]; then
-  if [[ -n "$2" && -n "$3" ]]; then
-    echo "generating a random key..."
+encrypt() {
+  echo "generating a random key..."
     openssl rand 192 -out key
-
     echo ""
-    openssl aes-256-cbc -in $2 -out $2.enc -pass file:key
-    openssl rsautl -encrypt -pubin -inkey $3 -in key -out key.enc
-    tar -zcvf $2.tgz *.enc
-    echo "Created $2.tgz"
+    openssl aes-256-cbc -in $1 -out $1.enc -pass file:key
+    openssl rsautl -encrypt -pubin -inkey $2 -in key -out key.enc
+    tar -zcvf $1.tgz *.enc
+    echo "Created $1.tgz"
     echo "Done."
-  fi
-elif [[ "$1" = "--decrypt" ]]; then
-  tar -xzvf $4
-  openssl rsautl -decrypt -ssl -inkey $3 -in key.enc -out key
-  openssl aes-256-cbc -d -in $2.enc -out $2 -pass file:key
-  rm key key.enc $2.enc
+}
+
+decrypt() {
+  tar -xzvf $3
+  openssl rsautl -decrypt -ssl -inkey $2 -in key.enc -out key
+  openssl aes-256-cbc -d -in $1.enc -out $1 -pass file:key
+  rm key key.enc $1.enc
   echo "Done."
+}
+
+fix() {
+  ssh-keygen -e -f $1 -m PKCS8 > ~/.ssh/$1.pkcs8
+}
+
+if [[ "$1" = "--encrypt" && -n "$2" && -n "$3" ]]; then
+  encrypt $2 $3
+elif [[ "$1" = "--decrypt" && -n "$2" && -n "$3" && -n "$4" ]]; then
+  decrypt $2 $3 $4
+elif [[ "$1" = "--fix" && -n "$2" ]]; then
+  fix $2
 else
   usage
 fi
